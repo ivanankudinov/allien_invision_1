75ea039047e7a8cdc0cfe3916032befc5855a1b1
Ivan Ankudinov <ivan.ankudinov97@gmail.com>
2022-11-17 19:40:09 +0300

    Оптимизация кнопки Play

--- first/alien_invasion.py
+++ first/alien_invasion.py
@@ -83,8 +83,16 @@ class AlienInvasion:
                 self._check_play_button(mouse_pos)
 
     def _check_play_button(self,mouse_pos):
-        if self.play_button.rect.collidepoint(mouse_pos):
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
+
+            self.stats.reset_stats()
             self.stats.game_active = True
+            self.aliens.empty()
+            self.bullets.empty()
+
+            self._create_fleet()
+            self.ship.center_ship()
 
     def _check_keydown_events(self,event):
         if event.key == pygame.K_RIGHT:
