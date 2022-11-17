d41cb42517b6464847b0dee847a8bf217fbdc1a1
Ivan Ankudinov <ivan.ankudinov97@gmail.com>
2022-11-17 19:27:30 +0300

    Запуск игры с кнопки Play

--- first/alien_invasion.py
+++ first/alien_invasion.py
@@ -78,6 +78,14 @@ class AlienInvasion:
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
 
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+
+    def _check_play_button(self,mouse_pos):
+        if self.play_button.rect.collidepoint(mouse_pos):
+            self.stats.game_active = True
+
     def _check_keydown_events(self,event):
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = True
