08d87c3e9db4d21fffdffac59a8e013f3f36e479
Ivan Ankudinov <ivan.ankudinov97@gmail.com>
2022-11-17 14:21:54 +0300

    Определение исполняемой части игры

--- first/alien_invasion.py
+++ first/alien_invasion.py
@@ -33,11 +33,12 @@ class AlienInvasion:
 
         while True:
             self._check_events()
+            if self.stats.game_active:
+                self.ship.update()
+                self.bullets.update()
+                self._update_bullets()
+                self._update_aliens()
             self._update_screen()
-            self.ship.update()
-            self.bullets.update()
-            self._update_bullets()
-            self._update_aliens()
 
 
 
