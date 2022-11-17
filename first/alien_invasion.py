70fc797ed1fb8e5fe047cd3c84db9dbe76d3d44c
Ivan Ankudinov <ivan.ankudinov97@gmail.com>
2022-11-17 20:48:35 +0300

    Сброс настроек при старте новой игры

--- first/alien_invasion.py
+++ first/alien_invasion.py
@@ -58,6 +58,7 @@ class AlienInvasion:
         if not self.aliens:
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _update_aliens(self):
         self._check_fleet_edges()
@@ -86,6 +87,7 @@ class AlienInvasion:
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.stats.game_active:
 
+            self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.stats.game_active = True
             self.aliens.empty()
